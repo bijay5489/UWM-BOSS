23a9afe0646e602fc05a7aacf37592e4bd1ceb2e
Mohammad Alsheikh <112969254+AlsheikhMohammmad@users.noreply.github.com>
2024-11-15 13:37:24 -0600

    stuff 9.0

--- Frontend/userInterface/styles/Settings.ts
+++ Frontend/userInterface/styles/Settings.ts
@@ -1,7 +1,7 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    userInfoContainer: {backgroundColor: '#f0f0f0', padding: 10, borderRadius: 5, marginBottom: 20,},
+    userInfoContainer: {backgroundColor: '#e3e3e3', padding: 10, borderRadius: 5, marginBottom: 20,},
     userInfoRow: {flexDirection: 'row', alignItems: 'center', marginBottom: 5,},
     userInfoText: {fontSize: 16, color: '#333', marginLeft: 10,},
     button: {flexDirection: 'row', alignItems: 'center', backgroundColor: '#4CAF50', padding: 15, marginBottom: 10, borderRadius: 10,},
