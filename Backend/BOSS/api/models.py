63ea2123a0ed5a95b7d1bfc7907041d4ce21de44
Mohammad Alsheikh <112969254+AlsheikhMohammmad@users.noreply.github.com>
2024-12-05 19:23:57 -0600

    Stuff 10.0

--- Backend/BOSS/api/models.py
+++ Backend/BOSS/api/models.py
@@ -54,7 +54,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 class Van(models.Model):
     van_number = models.CharField(max_length=15)
     ADA = models.BooleanField(default=False)
-    driver = models.ForeignKey(User, on_delete=models.CASCADE)
+    driver = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"Van {self.van_number} - ADA: {self.ADA}"
